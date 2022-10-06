b8d9f62c7b3b6ea52a8a8d4a651242b86ac92202
ITAKEB <tomascf123@gmail.com>
2022-10-05 22:57:42 -0500

    Implement first http response and body extraction

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use tokio::{
 
 use std::io::Cursor;
 
-
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
@@ -36,11 +35,42 @@ async fn main() -> Result<()> {
         }
         println!("-------------------------");
         println!("request: {:?}", request);
-        
-        //Google IP Adress on port 80 (http comunication)
-        let mut server = TcpStream::connect("	172.253.115.91:80").await?;
 
-        server.write_all(request.as_bytes()).await?;        
+        //Google IP Adress on port 80 (http comunication)
+        let mut server = TcpStream::connect("172.253.115.91:80").await?;
+
+        server.write_all(request.as_bytes()).await?;
+
+        let mut reader = BufReader::new(server);
+        let mut response = String::new();
+        reader.read_line(&mut response).await?;
+        loop {
+            reader.read_line(&mut response).await?;
+            if response.ends_with("\r\n\r\n") {
+                break;
+            }
+        }
+        println!("-------------------------");
+        println!("response: {:?}", response);
+
+        let mut cursor = Cursor::new(reader.buffer());
+        let mut body = vec![];
+        loop {
+            let num_bytes = cursor
+                .read_until(b'-', &mut body)
+                .await
+                .expect("reading from cursor won't fail");
+
+            if num_bytes == 0 {
+                break;
+            }
+        }
+        println!("-------------------------");
+        let b = String::from_utf8(body)?;
+        println!("body = {:?}", b);
+
+        response.push_str(&b);
+        stream.write_all(response.as_bytes()).await?;
     }
 
     Ok(())
