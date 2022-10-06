bfa2dd2ea5b8ad811868b09bcf979d03b3956ccd
ITAKEB <tomascf123@gmail.com>
2022-10-05 22:40:50 -0500

    Implement http request query and tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::*;
+use std::borrow::BorrowMut;
+use std::result::Result::Ok;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+use std::io::Cursor;
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    loop {
+        let (mut stream, _addr) = listener.accept().await?;
+        //println!("new client: {:?}", stream);
+        println!("{:?}", stream.local_addr()?);
+        let strm = stream.borrow_mut();
+        let mut reader = BufReader::new(strm);
+
+        let mut request = String::new();
+        reader.read_line(&mut request).await?;
+        //Replace Host
+        let mut host = String::new();
+        reader.read_line(&mut host).await?;
+        host = "Host: 172.253.115.91\r\n".to_owned();
+
+        request.push_str(&host);
+        loop {
+            reader.read_line(&mut request).await?;
+            if request.ends_with("\r\n\r\n") {
+                break;
+            }
+        }
+        println!("-------------------------");
+        println!("request: {:?}", request);
+    }
+
+    Ok(())
 }
