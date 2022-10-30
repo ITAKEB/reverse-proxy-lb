79042a0f6677154068b3c1b8b14ea1d45e98fb25
Danilo Toro <90995614+nerthust@users.noreply.github.com>
2022-10-30 18:17:09 -0500

    Update bash script

--- first-install-and-run.sh
+++ first-install-and-run.sh
@@ -3,9 +3,11 @@
 set -xeuf -o pipefail
 
 sudo apt update
+sudo chmod -R a+rwx ./
 sudo apt install curl build-essential
 curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
 
 rustup self update
 rustup default stable
+mkdir -p cachefiles
 cargo run --release
